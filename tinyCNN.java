5c43a6509bcb654d69e209e2c4a951cb991e7588
nthoangcute <40049635+nthoangcute@users.noreply.github.com>
2018-07-30 21:53:13 +0800

    Update parallel stream

--- tinyCNN.java
+++ tinyCNN.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.nio.file.*;
 import java.util.*;
+import java.util.function.*;
 import java.util.stream.*;
 
 class Layer {
@@ -48,7 +49,8 @@ public class tinyCNN {
 			}
 			update();
 			batchIndex = 0;
-			if (i % 100 == 0) System.out.println("\nStep\t" + i + " \tTestAccuracy =\t" + testPredict() + "%\tLearningRate =\t" + learningRate);
+			if (i % 100 == 0)
+				System.out.println("\nStep\t" + i + " \tTestAccuracy =\t" + testPredict() + "%\tLearningRate =\t" + learningRate);
 			else System.out.print(".");
 		}
 	}
@@ -85,8 +87,8 @@ public class tinyCNN {
 		for (String line : lines) {
 			double[] arr = Arrays.stream(line.split("\\|")).mapToDouble(Double::parseDouble).toArray();
 			int k = 0;
-			for (int i = 0; i < 28; i++) 
-				for (int j = 0; j < 28; j++) 
+			for (int i = 0; i < 28; i++)
+				for (int j = 0; j < 28; j++)
 					data[index][i][j] = arr[k++];
 			label[index++] = arr[k];
 		}
@@ -104,14 +106,17 @@ public class tinyCNN {
 		setupConv(layer6, layer5, outputSize, layer5.mapSize);
 	}
 
+	public void runProcess(int iLength, int jLength, BiConsumer<Integer, Integer> func) {
+		IntStream.range(0, iLength).parallel().forEach(i -> 
+			IntStream.range(0, jLength).parallel().forEach(j -> func.accept(i, j)));
+	}
+
 	public void setupConv(Layer layer, Layer pLayer, int mapNum, int size) {
 		layer.mapNum = mapNum;
 		layer.filterSize = size;
 		layer.mapSize = pLayer.mapSize - size + 1;
 		layer.filters = new double[pLayer.mapNum][mapNum][size][size];
-		for (int i = 0; i < pLayer.mapNum; i++) 
-			for (int j = 0; j < layer.mapNum; j++) 
-				layer.filters[i][j] = random(layer.filterSize, size);
+		runProcess(pLayer.mapNum, layer.mapNum, (i, j) -> layer.filters[i][j] = random(layer.filterSize, size));
 		layer.bias = new double[layer.mapNum];
 		layer.loss = new double[batchSize][layer.mapNum][layer.mapSize][layer.mapSize];
 		layer.maps = new double[batchSize][layer.mapNum][layer.mapSize][layer.mapSize];
@@ -140,19 +145,19 @@ public class tinyCNN {
 	}
 
 	public void setConvOutput(Layer layer, Layer pLayer) {
-		for (int j = 0; j < layer.mapNum; j++) {
+		IntStream.range(0, layer.mapNum).parallel().forEach(j -> {
 			double[][] sum = null;
 			for (int i = 0; i < pLayer.mapNum; i++) {
 				double[][] convn = conv(pLayer.maps[batchIndex][i], layer.filters[i][j]);
 				sum = (sum == null) ? convn : plus(convn, sum);
 			}
 			layer.maps[batchIndex][j] = sigmodBias(sum, layer.bias[j]);
-		}
+		});
 	}
 
 	public void setSampOutput(Layer layer, Layer pLayer) {
-		for (int i = 0; i < pLayer.mapNum; i++)
-			layer.maps[batchIndex][i] = scale(pLayer.maps[batchIndex][i], layer.scaleSize);
+		IntStream.range(0, pLayer.mapNum).parallel().forEach(i -> 
+			layer.maps[batchIndex][i] = scale(pLayer.maps[batchIndex][i], layer.scaleSize));
 	}
 
 	public void backPropagation(double label) {
@@ -168,31 +173,31 @@ public class tinyCNN {
 	}
 
 	public void setSampLoss(Layer layer, Layer nLayer) {
-		for (int i = 0; i < layer.mapNum; i++) {
+		IntStream.range(0, layer.mapNum).parallel().forEach(i -> {
 			double[][] sum = null;
 			for (int j = 0; j < nLayer.mapNum; j++) {
 				double[][] convFull = fullyConv(nLayer.loss[batchIndex][j], rotate(nLayer.filters[i][j]));
 				sum = (sum == null) ? convFull : plus(convFull, sum);
 			}
 			layer.loss[batchIndex][i] = sum;
-		}
+		});
 	}
 
 	public void setConvLoss(Layer layer, Layer nLayer) {
-		for (int m = 0; m < layer.mapNum; m++) {
+		IntStream.range(0, layer.mapNum).parallel().forEach(m -> {
 			double[][] nError = nLayer.loss[batchIndex][m];
 			double[][] outMatrix = mul2(layer.maps[batchIndex][m]);
 			layer.loss[batchIndex][m] = mul(outMatrix, kronecker(nError, nLayer.scaleSize));
-		}
+		});
 	}
 
 	public void setOutputLoss(double label) {
 		double[] target = new double[layer6.mapNum];
 		target[(int) label] = 1;
-		for (int m = 0; m < layer6.mapNum; m++) {
+		IntStream.range(0, layer6.mapNum).parallel().forEach(m -> {
 			double out = layer6.maps[batchIndex][m][0][0];
 			layer6.loss[batchIndex][m][0][0] = out * (1 - out) * (target[m] - out);
-		}
+		});
 	}
 
 	public void update() {
@@ -205,12 +210,12 @@ public class tinyCNN {
 	}
 
 	public void updateBias(Layer layer) {
-		for (int i = 0; i < layer.mapNum; i++)
-			layer.bias[i] += learningRate * sum(layer.loss, i) / batchSize;
+		IntStream.range(0, layer.mapNum).parallel().forEach(i -> {
+			layer.bias[i] += learningRate * sum(layer.loss, i) / batchSize;});
 	}
 
 	public void updateFilters(Layer layer, Layer pLayer) {
-		for (int i = 0; i < layer.mapNum; i++) 
+		IntStream.range(0, layer.mapNum).parallel().forEach(i -> {
 			for (int j = 0; j < pLayer.mapNum; j++) {
 				double[][] deltaF = null;
 				for (int k = 0; k < batchSize; k++) {
@@ -219,37 +224,29 @@ public class tinyCNN {
 				}
 				layer.filters[j][i] = mul3(layer.filters[j][i], div(deltaF));
 			}
+		});
 	}
 
 	public double[][] random(int x, int y) {
 		double[][] result = new double[x][y];
-		for (int i = 0; i < x; i++)
-			for (int j = 0; j < y; j++) 
-				result[i][j] = r.nextDouble() - 0.5;
+		runProcess(x, y, (i, j) -> result[i][j] = r.nextDouble() - 0.5);
 		return result;
 	}
 
 	public double[][] sigmodBias(double[][] m, double bias) {
-		for (double[] x : m)
-			for (int j = 0; j < m[0].length; j++)
-				x[j] = 1 / (1 + Math.pow(Math.E, -bias - x[j]));
+		runProcess(m.length, m[0].length, (i, j) -> m[i][j] = 1 / (1 + Math.pow(Math.E, -bias - m[i][j])));
 		return m;
 	}
 
 	public double[][] plus(double[][] x, double[][] y) {
-		for (int i = 0; i < x.length; i++)
-			for (int j = 0; j < x[0].length; j++)
-				y[i][j] += x[i][j];
+		runProcess(x.length, x[0].length, (i, j) -> y[i][j] += x[i][j]);
 		return y;
 	}
 
 	public double[][] scale(double[][] m, int s) {
 		double[][] result = new double[m.length / s][m[0].length / s];
-		for (int i = 0; i < m.length / s; i++) 
-			for (int j = 0; j < m[0].length / s; j++) 
-				for (int p = 0; p < s; p++) 
-					for (int q = 0; q < s; q++) 
-						result[i][j] += m[i * s + p][j * s + q] / s / s;
+		runProcess(m.length / s, m[0].length / s,
+				(i, j) -> runProcess(s, s, (p, q) -> result[i][j] += m[i * s + p][j * s + q] / s / s));
 		return result;
 	}
 
@@ -257,84 +254,58 @@ public class tinyCNN {
 		int x = m.length;
 		int y = m[0].length;
 		double[][] a = new double[x][y];
-		for (int i = 0; i < x; i++) System.arraycopy(m[i], 0, a[i], 0, y);
-		for (int i = 0; i < x; i++) 
-			for (int j = 0; j < y / 2; j++) 
-				a[i][j] = (a[i][j] + a[i][y - 1 - j]) - (a[i][y - 1 - j] = a[i][j]);
-		for (int j = 0; j < y; j++) 
-			for (int i = 0; i < x / 2; i++) 
-				a[i][j] = (a[i][j] + a[x - 1 - i][j]) - (a[x - 1 - i][j] = a[i][j]);
+		IntStream.range(0, x).parallel().forEach(i -> System.arraycopy(m[i], 0, a[i], 0, y));
+		runProcess(x, y / 2, (i, j) -> a[i][j] = (a[i][j] + a[i][y - 1 - j]) - (a[i][y - 1 - j] = a[i][j]));
+		runProcess(y, x / 2, (i, j) -> a[i][j] = (a[i][j] + a[x - 1 - i][j]) - (a[x - 1 - i][j] = a[i][j]));
 		return a;
 	}
 
 	public double[][] fullyConv(double[][] m, double[][] f) {
 		double[][] result = new double[m.length + 2 * f.length - 2][m[0].length + 2 * f[0].length - 2];
-		for (int i = 0; i < m.length; i++) 
-			for (int j = 0; j < m[0].length; j++) 
-				result[i + f.length - 1][j + f[0].length - 1] = m[i][j];
+		runProcess(m.length, m[0].length, (i, j) -> result[i + f.length - 1][j + f[0].length - 1] = m[i][j]);
 		return conv(result, f);
 	}
 
 	public double[][] mul(double[][] x, double[][] y) {
-		for (int i = 0; i < x.length; i++)
-			for (int j = 0; j < x[0].length; j++)
-				y[i][j] *= x[i][j];
+		runProcess(x.length, x[0].length, (i, j) -> y[i][j] *= x[i][j]);
 		return y;
 	}
 
 	public double[][] kronecker(double[][] m, int s) {
 		double[][] result = new double[m.length * s][m[0].length * s];
-		for (int i = 0; i < m.length; i++)
-			for (int j = 0; j < m[0].length; j++) 
-				for (int p = 0; p < s; p++)
-					for (int q = 0; q < s; q++)
-						result[i * s + p][j * s + q] = m[i][j];
+		runProcess(m.length, m[0].length, (i, j) -> runProcess(s, s, (p, q) -> result[i * s + p][j * s + q] = m[i][j]));
 		return result;
 	}
 
 	public double[][] mul2(double[][] x) {
 		double[][] y = new double[x.length][x[0].length];
-		for (int i = 0; i < x.length; i++)
-			for (int j = 0; j < x[0].length; j++)
-				y[i][j] = x[i][j] * (1 - x[i][j]);
+		runProcess(x.length, x[0].length, (i, j) -> y[i][j] = x[i][j] * (1 - x[i][j]));
 		return y;
 	}
 
 	public double[][] div(double[][] m) {
-		for (double[] x : m)
-			for (int j = 0; j < m[0].length; j++)
-				x[j] = x[j] / batchSize;
+		runProcess(m.length, m[0].length, (i, j) -> m[i][j] = m[i][j] / batchSize);
 		return m;
 	}
 
 	public double[][] mul3(double[][] x, double[][] y) {
-		for (int i = 0; i < x.length; i++)
-			for (int j = 0; j < x[0].length; j++)
-				y[i][j] = x[i][j] + y[i][j] * learningRate;
+		runProcess(x.length, x[0].length, (i, j) -> y[i][j] = x[i][j] + y[i][j] * learningRate);
 		return y;
 	}
 
 	public double sum(double[][][][] arr, int x) {
-		double sum = 0;
-		for (int i = 0; i < arr[0][x].length; i++)
-			for (int j = 0; j < arr[0][x][0].length; j++) 
-				for (double[][][] s : arr) 
-					sum += s[x][i][j];
-		return sum;
+		double[] sum = new double[1];
+		runProcess(arr[0][x].length, arr[0][x][0].length, (i, j) -> 
+			IntStream.range(0, arr.length).parallel().forEach(k -> sum[0] += arr[k][x][i][j]));
+		return sum[0];
 	}
 
 	public double[][] conv(double[][] m, double[][] f) {
 		int x = m.length - f.length + 1;
 		int y = m[0].length - f[0].length + 1;
 		double[][] result = new double[x][y];
-		for (int i = 0; i < x; i++)
-			for (int j = 0; j < y; j++) {
-				double sum = 0.0;
-				for (int p = 0; p < f.length; p++)
-					for (int q = 0; q < f[0].length; q++)
-						sum += m[i + p][j + q] * f[p][q];
-				result[i][j] = sum;
-			}
+		runProcess(x, y, (i, j) ->
+			runProcess(f.length, f[0].length, (p,q)->result[i][j] += m[i + p][j + q] * f[p][q]));
 		return result;
 	}
 }
